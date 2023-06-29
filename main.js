f858a51a743df0aecec408c2eb28990cf868c0a0
m-sato <ssmmaagg36@gmail.com>
2023-06-29 10:08:18 +0900

    マテリアルの基礎

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function init() {
 
   // カメラの追加
   const camera = new THREE.PerspectiveCamera(50, window.innerWidth / innerHeight, 0.1, 1000);
-  camera.position.set(0, 0, 500);
+  camera.position.set(0, 0, 10);
 
   // レンダラーの追加
   const renderer = new THREE.WebGLRenderer({
@@ -22,48 +22,44 @@ function init() {
   // レンダリングしたいDOM要素と紐付け
   document.body.appendChild(renderer.domElement);
 
-  // テクスチャの追加
-  const texture = new THREE.TextureLoader().load("./textures/earth.jpg");
-
   // ジオメトリ作成
-  const ballGeometry = new THREE.SphereGeometry(100, 64, 32);
+  const sphereGeometry = new THREE.SphereGeometry();
+  const planeGeometry = new THREE.PlaneGeometry(2, 2);
+  const octahedronGeometry = new THREE.OctahedronGeometry(1);
 
   // マテリアル作成
-  const ballMaterial = new THREE.MeshPhysicalMaterial({ map: texture });
+  // const material = new THREE.MeshBasicMaterial({
+  //   color: "#cecece",
+  //   wireframe: true,
+  // });
+  const material = new THREE.MeshPhongMaterial({
+    color: "#ede5ff",
+    emissive: "#361249",
+    speculay: "#ffadad",
+    shininess: 100,
+    flatShading: true,
+    fog: true,
+    envMaps: "reflection",
+    reflectivity: 1,
+    refractionRatio: 1,
+  });
+
+  material.side = THREE.DoubleSide;
+
+  // 光源
+  const ambientLight = new THREE.AmbientLight("#ffffff", 0.7);
+  const pointLight = new THREE.PointLight("#ffffff", 1);
+  pointLight.position.set(1, 2, 3);
+  const pointLightHelper = new THREE.PointLightHelper(pointLight, 1);
+  scene.add(ambientLight, pointLight, pointLightHelper);
 
   // メッシュ化
-  const ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
-  scene.add(ballMesh);
-
-  // 平行光源の追加
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
-  // 光の当たる位置調整
-  directionalLight.position.set(1, 1, 1);
-  // シーンに追加
-  scene.add(directionalLight);
-
-  // ポイント光源を追加
-  const pointLight = new THREE.PointLight(0xffffff, 1);
-  pointLight.position.set(-200, -200, -200);
-  scene.add(pointLight);
-
-  // マウス操作
-  const Controls = new OrbitControls(camera, renderer.domElement);
-
-  // ポイント光源のアニメーション
-  function animate() {
-    // ポジションを動的に指定
-    pointLight.position.set(
-      200 * Math.sin(Date.now() / 500),
-      200 * Math.sin(Date.now() / 1000),
-      200 * Math.cos(Date.now() / 500)
-    );
-    // フレーム単位で関数実行
-    requestAnimationFrame(animate);
-
-    // レンダリング関数
-    renderer.render(scene, camera);
-  }
+  const sphere = new THREE.Mesh(sphereGeometry, material);
+  const plane = new THREE.Mesh(planeGeometry, material);
+  const octahedron = new THREE.Mesh(octahedronGeometry, material);
+  sphere.position.x = -3;
+  octahedron.position.x = 3;
+  scene.add(sphere, plane, octahedron);
 
   // ブラウザリサイズ対応
   function onWindowResize() {
@@ -72,6 +68,25 @@ function init() {
     camera.updateProjectionMatrix();
   }
 
+  const clock = new THREE.Clock();
+
+  function animate() {
+    // オブジェクトの回転
+    const elapsedTime = clock.getElapsedTime();
+    sphere.rotation.x = elapsedTime;
+    octahedron.rotation.x = elapsedTime;
+    plane.rotation.x = elapsedTime;
+    sphere.rotation.y = elapsedTime;
+    octahedron.rotation.y = elapsedTime;
+    plane.rotation.y = elapsedTime;
+
+    // レンダリング
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+  }
+
   window.addEventListener("resize", onWindowResize);
+  // マウス操作
+  const Controls = new OrbitControls(camera, renderer.domElement);
   animate();
 }
