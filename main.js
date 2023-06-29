df67db5b5cf00ddf60d57c2419f4aeef249e1cb9
m-sato <ssmmaagg36@gmail.com>
2023-06-29 16:12:35 +0900

    カメラの基礎

--- main.js
+++ main.js
@@ -4,74 +4,61 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 window.addEventListener("load", init);
 
 function init() {
-  // シーンの追加
+  let coursorX, coursorY;
+  window.addEventListener("mousemove", (event) => {
+    coursorX = event.clientX / sizes.width - 0.5;
+    coursorY = event.clientY / sizes.height - 0.5;
+  });
+
+  //サイズ
+  const sizes = {
+    width: 800,
+    height: 600,
+  };
+
+  //シーン
   const scene = new THREE.Scene();
 
-  // カメラの追加
-  const camera = new THREE.PerspectiveCamera(50, window.innerWidth / innerHeight, 0.1, 1000);
-  camera.position.set(0, 0, 500);
-
-  // レンダラーの追加
-  const renderer = new THREE.WebGLRenderer({
-    alpha: true,
+  const geometry = new THREE.BoxGeometry(1, 1, 1, 5, 5, 5);
+  const materila = new THREE.MeshBasicMaterial({
+    color: 0x00ffff,
+    wireframe: false,
   });
-  // サイズ調整
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  // 画質調整
-  renderer.setPixelRatio(window.devicePixelRatio);
-  // レンダリングしたいDOM要素と紐付け
+
+  //オブジェクト
+  const mesh = new THREE.Mesh(geometry, materila);
+  scene.add(mesh);
+
+  //カメラ
+  const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+
+  camera.position.z = 3;
+  scene.add(camera);
+
+  //レンダラー
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(sizes.width, sizes.height);
   document.body.appendChild(renderer.domElement);
 
-  // テクスチャの追加
-  const texture = new THREE.TextureLoader().load("./textures/earth.jpg");
+  // カメラ制御
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  //アニメーション
+  const animate = () => {
+    //カメラ制御
+    // camera.position.x = coursorX * 3;
+    // camera.position.y = coursorY * 3;
+    // camera.position.x = Math.sin(Math.PI * 2 * coursorX) * 3;
+    // camera.position.z = Math.cos(Math.PI * 2 * coursorX) * 3;
+    // camera.position.y = coursorY + 5;
+    // camera.lookAt(mesh.position);
 
-  // ジオメトリ作成
-  const ballGeometry = new THREE.SphereGeometry(100, 64, 32);
+    controls.update();
 
-  // マテリアル作成
-  const ballMaterial = new THREE.MeshPhysicalMaterial({ map: texture });
-
-  // メッシュ化
-  const ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
-  scene.add(ballMesh);
-
-  // 平行光源の追加
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
-  // 光の当たる位置調整
-  directionalLight.position.set(1, 1, 1);
-  // シーンに追加
-  scene.add(directionalLight);
-
-  // ポイント光源を追加
-  const pointLight = new THREE.PointLight(0xffffff, 1);
-  pointLight.position.set(-200, -200, -200);
-  scene.add(pointLight);
-
-  // マウス操作
-  const Controls = new OrbitControls(camera, renderer.domElement);
-
-  // ポイント光源のアニメーション
-  function animate() {
-    // ポジションを動的に指定
-    pointLight.position.set(
-      200 * Math.sin(Date.now() / 500),
-      200 * Math.sin(Date.now() / 1000),
-      200 * Math.cos(Date.now() / 500)
-    );
-    // フレーム単位で関数実行
-    requestAnimationFrame(animate);
-
-    // レンダリング関数
+    //レンダリング
     renderer.render(scene, camera);
-  }
+    window.requestAnimationFrame(animate);
+  };
 
-  // ブラウザリサイズ対応
-  function onWindowResize() {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-  }
-
-  window.addEventListener("resize", onWindowResize);
   animate();
 }
